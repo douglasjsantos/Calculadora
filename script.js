75589d185e759f3d0b7fe42b0e2011c2bbae66e2
Douglas <douglas_j.santos@hotmail.com>
2023-04-27 20:25:16 -0300

    arrumando css e introduzindo javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+const span = document.querySelector("span");
+const botoes = document.querySelectorAll("button");
+const remocaoTotal = document.querySelector(".C");
+const mais = document.querySelector(".mais");
+const um = document.querySelector(".um");
